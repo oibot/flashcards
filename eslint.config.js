69add3cce320816ea03533891de3084ae31cc9bb
Tobias Ostner <tobias.ostner@gmail.com>
2026-03-05 09:11:23 +0100

    chore(eslint): add react plugin config

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,24 @@
 const { defineConfig } = require("eslint/config")
 const expoConfig = require("eslint-config-expo/flat")
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended")
+const react = require("eslint-plugin-react")
 const simpleImportSort = require("eslint-plugin-simple-import-sort")
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
   {
     ignores: ["dist/*", "src/locales/**/*"],
   },
   {
     plugins: {
+      react,
       "simple-import-sort": simpleImportSort,
+    },
+    settings: {
+      react: { version: "detect" },
     },
     rules: {
       "simple-import-sort/imports": "error",
