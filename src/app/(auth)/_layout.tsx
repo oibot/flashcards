9864e1cad195e5f527fb3d4b56816a2af3d68381
Tobias Ostner <tobias.ostner@gmail.com>
2026-04-02 14:49:35 +0200

    refactor(auth): rely on root navigation theme

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,21 +1,13 @@
 import { Stack } from "expo-router"
-import { StyleSheet } from "react-native-unistyles"
 
 export default function Layout() {
   return (
     <Stack
       screenOptions={{
         headerShown: false,
-        contentStyle: styles.content,
       }}
     >
       <Stack.Screen name="index" />
     </Stack>
   )
 }
-
-const styles = StyleSheet.create((theme) => ({
-  content: {
-    backgroundColor: theme.colors.background,
-  },
-}))
